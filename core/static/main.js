c9260b16570570960417309ba0a01c97b3fc2e2e
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-10-23 22:56:24 +0200

    Refactor addMeasurePoints

--- core/static/main.js
+++ core/static/main.js
@@ -91,64 +91,8 @@ beforeMap.on('load', () => {
     afterMap.addControl(new mapboxgl.NavigationControl());
 
     addKeyboardControl(afterMap);
+    addMeasurePoints(beforeMap);
 
-});
-
-beforeMap.on('click', (e) => {
-    const features = beforeMap.queryRenderedFeatures(e.point, {
-        layers: ['measure-points']
-    });
-
-    // Remove the linestring from the group
-    // so we can redraw it based on the points collection.
-    if (geojson.features.length > 1) geojson.features.pop();
-
-    // Clear the distance container to populate it with a new value.
-    distanceContainer.innerHTML = '';
-
-    // If a feature was clicked, remove it from the beforeMap.
-    if (features.length) {
-        const id = features[0].properties.id;
-        geojson.features = geojson.features.filter((point) => point.properties.id !== id);
-    } else {
-        const point = {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [e.lngLat.lng, e.lngLat.lat]
-            },
-            'properties': {
-                'id': String(new Date().getTime())
-            }
-        };
-
-        geojson.features.push(point);
-    }
-
-    if (geojson.features.length > 1) {
-        linestring.geometry.coordinates = geojson.features.map(
-            (point) => point.geometry.coordinates
-        );
-
-        geojson.features.push(linestring);
-
-        // Populate the distanceContainer with total distance
-        const value = document.createElement('pre');
-        const distance = turf.length(linestring);
-        value.textContent = `Total distance: ${distance.toLocaleString()}km`;
-        distanceContainer.appendChild(value);
-    }
-
-    beforeMap.getSource('geojson').setData(geojson);
-});
-
-beforeMap.on('mousemove', (e) => {
-    const features = beforeMap.queryRenderedFeatures(e.point, {
-        layers: ['measure-points']
-    });
-    // Change the cursor to a pointer when hovering over a point on the beforeMap.
-    // Otherwise cursor is a crosshair.
-    beforeMap.getCanvas().style.cursor = features.length ? 'pointer': 'crosshair';
 });
 
 beforeMap.on('click', 'zrzutymerge-btmnix', (e) => {
@@ -227,5 +171,64 @@ function addKeyboardControl(map){
         },
         true
     );
+}
 
+function addMeasurePoints(map){
+
+    map.on('click', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+            layers: ['measure-points']
+        });
+
+        // Remove the linestring from the group
+        // so we can redraw it based on the points collection.
+        if (geojson.features.length > 1) geojson.features.pop();
+
+        // Clear the distance container to populate it with a new value.
+        distanceContainer.innerHTML = '';
+
+        // If a feature was clicked, remove it from the map.
+        if (features.length) {
+            const id = features[0].properties.id;
+            geojson.features = geojson.features.filter((point) => point.properties.id !== id);
+        } else {
+            const point = {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [e.lngLat.lng, e.lngLat.lat]
+                },
+                'properties': {
+                    'id': String(new Date().getTime())
+                }
+            };
+
+            geojson.features.push(point);
+        }
+
+        if (geojson.features.length > 1) {
+            linestring.geometry.coordinates = geojson.features.map(
+                (point) => point.geometry.coordinates
+            );
+
+            geojson.features.push(linestring);
+
+            // Populate the distanceContainer with total distance
+            const value = document.createElement('pre');
+            const distance = turf.length(linestring);
+            value.textContent = `Total distance: ${distance.toLocaleString()}km`;
+            distanceContainer.appendChild(value);
+        }
+
+        map.getSource('geojson').setData(geojson);
+    });
+
+    map.on('mousemove', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+            layers: ['measure-points']
+        });
+        // Change the cursor to a pointer when hovering over a point on the map.
+        // Otherwise cursor is a crosshair.
+        map.getCanvas().style.cursor = features.length ? 'pointer': 'crosshair';
+    });
 }
