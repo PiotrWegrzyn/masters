2a7ed051a414c1b6fe57a109081588d89aafcd68
piotr-wegrzyn <piotr.wegrzyn@instream.io>
2021-11-13 01:26:53 +0100

    Add controls.

--- core/static/main.js
+++ core/static/main.js
@@ -75,7 +75,24 @@ beforeMap.on('load', () => {
     DistanceCalculator.attach(soloMap);
 
     hideMap('comparison-map-container');
-    // hideMap('solo-map-container');
+    soloMap.addControl(new mapboxgl.FullscreenControl());    // hideMap('solo-map-container');
+    soloMap.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            // When active the map will receive updates to the device's location as it changes.
+            trackUserLocation: true,
+            // Draw an arrow next to the location dot to indicate which direction the device is heading.
+            showUserHeading: true
+            })
+    );   // hideMap('solo-map-container');
+    const scale = new mapboxgl.ScaleControl({
+        maxWidth: 80,
+        unit: 'metric'
+    });
+    soloMap.addControl(scale);
+
+
 
 });
 
